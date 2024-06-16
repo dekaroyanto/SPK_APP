bb00fd651fb20ff58c90e0dff27421f0b8819b1d
dekaroyanto <dekaroy05@gmail.com>
2024-06-16 22:56:13 +0700

    [CHANGE] Laptop

---
+++ Database/SPK_APP-1baru.sql
@@ -0,0 +1,343 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 09, 2024 at 05:46 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `spk_roc_vikor_laravel_10`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `alternatif`
+--
+
+CREATE TABLE `alternatif` (
+  `id_alternatif` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `divisi` enum('marketing','collection') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `alternatif`
+--
+
+INSERT INTO `alternatif` (`id_alternatif`, `nama`, `divisi`) VALUES
+(72, 'test divisi collection', 'collection'),
+(73, 'deka', 'marketing'),
+(74, 'yaris', 'marketing');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hasil`
+--
+
+CREATE TABLE `hasil` (
+  `id_hasil` int(11) NOT NULL,
+  `id_alternatif` int(11) NOT NULL,
+  `nilai` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `hasil`
+--
+
+INSERT INTO `hasil` (`id_hasil`, `id_alternatif`, `nilai`) VALUES
+(1, 72, 1),
+(2, 73, 0.5),
+(3, 74, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kriteria`
+--
+
+CREATE TABLE `kriteria` (
+  `id_kriteria` int(11) NOT NULL,
+  `keterangan` varchar(100) NOT NULL,
+  `kode_kriteria` varchar(100) NOT NULL,
+  `bobot` float DEFAULT NULL,
+  `prioritas` char(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `kriteria`
+--
+
+INSERT INTO `kriteria` (`id_kriteria`, `keterangan`, `kode_kriteria`, `bobot`, `prioritas`) VALUES
+(56, 'Pengalaman', 'K1', 0.456667, '1'),
+(57, 'Usia', 'K2', 0.256667, '2'),
+(58, 'Tes Wawancara', 'K3', 0.156667, '3'),
+(59, 'Tes Kemampuan', 'K4', 0.09, '4'),
+(60, 'Pendidikan', 'K5', 0.04, '5');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penilaian`
+--
+
+CREATE TABLE `penilaian` (
+  `id_penilaian` int(11) NOT NULL,
+  `id_alternatif` int(11) NOT NULL,
+  `id_kriteria` int(11) NOT NULL,
+  `nilai` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `penilaian`
+--
+
+INSERT INTO `penilaian` (`id_penilaian`, `id_alternatif`, `id_kriteria`, `nilai`) VALUES
+(473, 72, 56, 278),
+(474, 72, 57, 283),
+(475, 72, 58, 288),
+(476, 72, 59, 293),
+(477, 72, 60, 298),
+(480, 73, 56, 279),
+(481, 73, 57, 284),
+(482, 73, 58, 289),
+(483, 73, 59, 294),
+(484, 73, 60, 299),
+(487, 74, 56, 280),
+(488, 74, 57, 285),
+(489, 74, 58, 290),
+(490, 74, 59, 295),
+(491, 74, 60, 300);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sub_kriteria`
+--
+
+CREATE TABLE `sub_kriteria` (
+  `id_sub_kriteria` int(11) NOT NULL,
+  `id_kriteria` int(11) NOT NULL,
+  `deskripsi` varchar(200) NOT NULL,
+  `nilai` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `sub_kriteria`
+--
+
+INSERT INTO `sub_kriteria` (`id_sub_kriteria`, `id_kriteria`, `deskripsi`, `nilai`) VALUES
+(278, 56, 'Tidak Ada', 1),
+(279, 56, '1 Tahun', 2),
+(280, 56, '2 Tahun', 3),
+(281, 56, '3 Tahun', 4),
+(282, 56, '>3 Tahun', 5),
+(283, 57, '20 Tahun', 1),
+(284, 57, '21 Tahun', 2),
+(285, 57, '24 Tahun', 3),
+(286, 57, '22 Tahun', 4),
+(287, 57, '23 Tahun', 5),
+(288, 58, 'Sangat Buruk', 1),
+(289, 58, 'Buruk', 2),
+(290, 58, 'Cukup', 3),
+(291, 58, 'Bagus', 4),
+(292, 58, 'Sangat Bagus', 5),
+(293, 59, 'Sangat Buruk', 1),
+(294, 59, 'Buruk', 2),
+(295, 59, 'Cukup', 3),
+(296, 59, 'Bagus', 4),
+(297, 59, 'Sangat Bagus', 5),
+(298, 60, 'SD', 1),
+(299, 60, 'SMP', 2),
+(300, 60, 'SMA', 3),
+(301, 60, 'D3', 4),
+(302, 60, 'S1', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `id_user_level` int(11) NOT NULL,
+  `nama` varchar(200) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `id_user_level`, `nama`, `email`, `username`, `password`) VALUES
+(1, 1, 'Admin Update', 'admin@gmail.com', 'admin', '21232f297a57a5a743894a0e4a801fc3'),
+(7, 2, 'User', 'user@gmail.com', 'user', 'ee11cbb19052e40b07aac0ca060c23ee'),
+(9, 1, 'Deka Royanto', 'deka@gmail.com', 'deka', '57ef16a773d505292b52918bcd6d8d29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_level`
+--
+
+CREATE TABLE `user_level` (
+  `id_user_level` int(11) NOT NULL,
+  `user_level` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `user_level`
+--
+
+INSERT INTO `user_level` (`id_user_level`, `user_level`) VALUES
+(1, 'Administrator'),
+(2, 'User');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `alternatif`
+--
+ALTER TABLE `alternatif`
+  ADD PRIMARY KEY (`id_alternatif`);
+
+--
+-- Indexes for table `hasil`
+--
+ALTER TABLE `hasil`
+  ADD PRIMARY KEY (`id_hasil`),
+  ADD KEY `id_alternatif` (`id_alternatif`);
+
+--
+-- Indexes for table `kriteria`
+--
+ALTER TABLE `kriteria`
+  ADD PRIMARY KEY (`id_kriteria`);
+
+--
+-- Indexes for table `penilaian`
+--
+ALTER TABLE `penilaian`
+  ADD PRIMARY KEY (`id_penilaian`),
+  ADD KEY `id_alternatif` (`id_alternatif`),
+  ADD KEY `id_kriteria` (`id_kriteria`),
+  ADD KEY `nilai` (`nilai`);
+
+--
+-- Indexes for table `sub_kriteria`
+--
+ALTER TABLE `sub_kriteria`
+  ADD PRIMARY KEY (`id_sub_kriteria`),
+  ADD KEY `id_kriteria` (`id_kriteria`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `id_user_level` (`id_user_level`);
+
+--
+-- Indexes for table `user_level`
+--
+ALTER TABLE `user_level`
+  ADD PRIMARY KEY (`id_user_level`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `alternatif`
+--
+ALTER TABLE `alternatif`
+  MODIFY `id_alternatif` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
+
+--
+-- AUTO_INCREMENT for table `hasil`
+--
+ALTER TABLE `hasil`
+  MODIFY `id_hasil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `kriteria`
+--
+ALTER TABLE `kriteria`
+  MODIFY `id_kriteria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+
+--
+-- AUTO_INCREMENT for table `penilaian`
+--
+ALTER TABLE `penilaian`
+  MODIFY `id_penilaian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=494;
+
+--
+-- AUTO_INCREMENT for table `sub_kriteria`
+--
+ALTER TABLE `sub_kriteria`
+  MODIFY `id_sub_kriteria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=318;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `user_level`
+--
+ALTER TABLE `user_level`
+  MODIFY `id_user_level` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `hasil`
+--
+ALTER TABLE `hasil`
+  ADD CONSTRAINT `hasil_ibfk_1` FOREIGN KEY (`id_alternatif`) REFERENCES `alternatif` (`id_alternatif`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `penilaian`
+--
+ALTER TABLE `penilaian`
+  ADD CONSTRAINT `penilaian_ibfk_1` FOREIGN KEY (`id_alternatif`) REFERENCES `alternatif` (`id_alternatif`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `penilaian_ibfk_2` FOREIGN KEY (`id_kriteria`) REFERENCES `kriteria` (`id_kriteria`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `penilaian_ibfk_3` FOREIGN KEY (`nilai`) REFERENCES `sub_kriteria` (`id_sub_kriteria`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `sub_kriteria`
+--
+ALTER TABLE `sub_kriteria`
+  ADD CONSTRAINT `sub_kriteria_ibfk_1` FOREIGN KEY (`id_kriteria`) REFERENCES `kriteria` (`id_kriteria`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id_user_level`) REFERENCES `user_level` (`id_user_level`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
